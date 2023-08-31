ab80c3bc321ae43a2332cbb6fd3953241af69971
stefkwan-h8 <82027557+stefkwan-h8@users.noreply.github.com>
2023-08-31 17:28:41 +0700

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,134 @@
+/*
+ * JavaScript file for the application to demonstrate
+ * using the API
+ */
+
+// Create the namespace instance
+let ns = {};
+
+// Create the model instance
+ns.model = (function() {
+    'use strict';
+
+    let $event_pump = $('body');
+
+    // Return the API
+    return {
+        'read': function() {
+            let ajax_options = {
+                type: 'GET',
+                url: 'api/people',
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_read_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        create: function(fname, lname) {
+            let ajax_options = {
+                type: 'POST',
+                url: 'api/people',
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    'fname': fname,
+                    'lname': lname
+                })
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_create_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        update: function(fname, lname) {
+            let ajax_options = {
+                type: 'PUT',
+                url: 'api/people/' + lname,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    'fname': fname,
+                    'lname': lname
+                })
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_update_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        'delete': function(lname) {
+            let ajax_options = {
+                type: 'DELETE',
+                url: 'api/people/' + lname,
+                accepts: 'application/json',
+                contentType: 'plain/text'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_delete_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        }
+    };
+}());
+
+// Create the view instance
+ns.view = (function() {
+    'use strict';
+
+    let $fname = $('#fname'),
+        $lname = $('#lname');
+
+    // return the API
+    return {
+        reset: function() {
+            $lname.val('');
+            $fname.val('').focus();
+        },
+        update_editor: function(fname, lname) {
+            $lname.val(lname);
+            $fname.val(fname).focus();
+        },
+        build_table: function(people) {
+            let rows = ''
+
+            // clear the table
+            $('.people table > tbody').empty();
+
+            // did we get a people array?
+            if (people) {
+                for (let i=0, l=people.length; i < l; i++) {
+                    rows += `<tr><td class="fname">${people[i].fname}</td><td class="lname">${people[i].lname}</td><td>${people[i].timestamp}</td></tr>`;
+                }
+                $('table > tbody').append(rows);
+            }
+        },
+        error: function(error_msg) {
+            $('.error')
+                .text(error_msg)
+                .css('visibility', 'visible');
+            setTimeout(function() {
+                $('.error').css('visibility', 'hidden');
+            }, 3000)
+        }
+    };
+}());
+
 // Create the controller
 ns.controller = (function(m, v) {
     'use strict';
